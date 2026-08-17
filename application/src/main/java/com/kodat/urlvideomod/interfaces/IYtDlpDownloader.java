5d23d2fb3539e6e277a73abae274c6990b7834ae
ondrakodat12 <revelcz0408@gmail.com>
2026-08-17 13:31:24 +0200

    Oprava toho co se pokazilo :/

--- application/src/main/java/com/kodat/urlvideomod/interfaces/IYtDlpDownloader.java
+++ application/src/main/java/com/kodat/urlvideomod/interfaces/IYtDlpDownloader.java
@@ -1,9 +1,10 @@
 package com.kodat.urlvideomod.interfaces;
 
+import com.kodat.urlvideomod.entity.DownloadFile;
 import com.kodat.urlvideomod.entity.YtDlpFileInfo;
 import com.kodat.urlvideomod.enums.TypeOfDownload;
 
 public interface IDownloader {
     YtDlpFileInfo getFileInfo(String url);
-    void download (String url, TypeOfDownload format);
+    DownloadFile download (String url, TypeOfDownload format);
 }
